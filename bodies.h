f7c040816e9378459b6bb956171a74bc8740e0ab
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2022-11-10 15:32:28 -0500

    Update bodies.h
    Implementacion clase vector 3d

--- bodies.h
+++ bodies.h
@@ -1,9 +1,49 @@
 #pragma once // avoids multiple inclusions
-#include <iostream>
-#include <vector>
 
-
-typedef std::vector<double> vec; //Nombre para vector
+struct Vec3D
+{
+    double x, y, z;
+    //Constructor
+    Vec3D(){
+        x = 0.0; y = 0.0; z = 0.0;
+    }
+    //Print vector
+    void print(){
+        std::cout << "(" << x << ", " << y << ", " << z << ")" << "\n";
+    }
+    //Overloading operator +
+    Vec3D operator+(const Vec3D &v){
+        Vec3D sum;
+        sum.x = x + v.x;
+        sum.y = y + v.y;
+        sum.z = z + v.z;
+        return sum;
+    }
+    //Overloading operator +
+    Vec3D operator-(const Vec3D &v){
+        Vec3D sum;
+        sum.x = x - v.x;
+        sum.y = y - v.y;
+        sum.z = z - v.z;
+        return sum;
+    }
+    //Overloading operator *
+    Vec3D operator*(const double &c){
+        Vec3D esc;
+        esc.x = c*x;
+        esc.y = c*y;
+        esc.z = c*z;
+        return esc;
+    }
+    //Overloading operator /
+    Vec3D operator/(const double &c){
+        Vec3D esc;
+        esc.x = x/c;
+        esc.y = y/c;
+        esc.z = z/c;
+        return esc;
+    }
+};
 
 struct Planeta
 {
@@ -12,12 +52,41 @@ struct Planeta
     //Masa
     double mass = 0.0;
     //Posicion
-    vec R = {0, 0 ,0};
+    Vec3D R;
     //Velocidad
-    vec V = {0, 0 ,0};
+    Vec3D V;
+    //Aceleracion auxiliar
+    Vec3D Aaux;
     //Fuerza
-    vec F = {0, 0 ,0};
-    //Vector energia (cinetica, potencial, mecanica)
-    vec E = {0, 0 ,0};
+    Vec3D F;
+    //Vector energia (cenetica, potencial, mecanica)
+    Vec3D E;
+
+    //Constructor condiciones iniciales
+    Planeta(double x0, double y0, double z0, double Vx0, double Vy0, double Vz0, double m){
+        this -> mass = m;
+        this -> R.x = x0;
+        this -> R.y = y0;
+        this -> R.z = z0;
+        this -> V.x = Vx0;
+        this -> V.y = Vy0;
+        this -> V.z = Vz0;
+    }
+
+    //Leap_Frog algorithm
+    void start_leap_frog(const double & dt){
+        V = V + F*(dt/(2*mass));
+    }
+
+    void leap_frog(const double & dt){
+        V = V + F*(dt/(mass));
+        R = R + V*dt;
+    }
+
+    //Verlet algorithm
+    void verlet(const double & dt){
+        R = R + V*dt + Aaux*(dt*dt*0.5);
+        V = V + (Aaux+(F/mass))*(dt*0.5);
+    }
 
 };
