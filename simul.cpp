4ede2edaa007615b838c25b0c67c2fbf30e2976f
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2022-11-24 14:57:20 -0500

    Update simul.cpp

--- simul.cpp
+++ simul.cpp
@@ -1,54 +1,56 @@
 #include "declarations.h"
 
-void force(Planeta & body1, Planeta & body2, Planeta & body3){
-
-    body1.Aaux = body1.F/body1.mass;
-    body2.Aaux = body2.F/body2.mass;
-    body3.Aaux = body3.F/body3.mass;
-    
-
-    Vec3D r12;
-    // direccion de la fuerza m1->m2
-    r12 = body2.R - body1.R;
-    
-    Vec3D r13;
-    // direccion de la fuerza m1->m3
-    r13 = body3.R - body1.R;
-    
-    Vec3D r23;
-    // direccion de la fuerza m1->m2
-    r23 = body3.R - body2.R;
-    
-    
-    //norma r12
-    double r12_norm = std::pow((r.x*r.x)+(r.y*r.y)+(r.z*r.z), -1.5);
-
-    double esc = G*body1.mass*body2.mass*r_norm; 
- 
-    // Fuerza
-    body1.F = r*esc;
-    body2.F = r*(-esc);
+void trans_galileo(cuerpos & Nbody){
+    int N = Nbody.size();
+    for(int ii = 0; ii < N; ++ii){
+        Nbody[ii].R = Nbody[ii].R - Nbody[N-1].R;
+        Nbody[ii].V = Nbody[ii].V - Nbody[N-1].V;
+    }
 }
 
-void trans_galileo(Planeta & bodyFijo, Planeta & body2, Planeta & body3){
-    
-    body2.R = body2.R - bodyFijo.R; 
-    body2.V = body2.V - bodyFijo.V;
-    
-    body3.R = body3.R - bodyFijo.R; 
-    body3.V = body3.V - bodyFijo.V;
-    
-    bodyFijo.R = bodyFijo.R - bodyFijo.R;
-    bodyFijo.V = bodyFijo.V - bodyFijo.V;
-    
+void force(cuerpos & Nbody){
+    int N = Nbody.size();
+
+    //Reiniciar fuerzas
+    for(int ii = 0; ii < N; ii++){
+        Nbody[ii].F.x = Nbody[ii].F.y = Nbody[ii].F.z = 0.0;
+    }
+    //Calcular fuerza
+    for(int ii = 0; ii < N; ii++){
+        for(int jj = 0; jj < N; jj++){
+            if(ii != jj){
+                Vec3D r;
+                // direccion de la fuerza m1->m2
+                r = Nbody[jj].R - Nbody[ii].R;
+                //norma
+                double r_norm = std::pow((r.x*r.x)+(r.y*r.y)+(r.z*r.z), -1.5);
+
+                // Fuerza - Ley de gravitacion universal
+                double esc = G*Nbody[ii].mass*Nbody[jj].mass*r_norm; 
+                Nbody[ii].F = Nbody[ii].F + r*esc;
+            }
+        }
+        //Actualizar posicion y velocidad
+        Nbody[ii].leap_frog(DT);
+    }
+}
+
+void print(cuerpos & Nbody, double time){
+    int N = Nbody.size();
+    //Imprimir resultados
+    std::cout << time << " , ";
+    for(int ii = 0; ii < N; ii++){
+        std::cout << Nbody[ii].R.x << " , " << Nbody[ii].R.y <<  " , ";
+    }
+    std::cout << "\n";
 }
 
 
-
-void print(Planeta & body1, Planeta & body2, double time){
-    std::cout << time << ","
-        << body1.R.x << ","
-        << body2.R.x << ","
-        << body1.R.y << ","
-        << body2.R.y << "\n";
+ bool gassist(cuerpos & Nbody, double Rp){
+    bool control = false;
+    double normb = sqrt(Nbody[0].R^Nbody[0].R); 
+    if(fabs(normb - Rp) <= eps){control = true;}
+    return control;
 }
+
+
