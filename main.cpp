53298e9b6edc9be82c9a3fe9ab6b5bedc284f4c4
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2022-11-24 14:54:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,53 @@
 #include "declarations.h"
 
 int main(){
+    //Datos Sol
+    double mS = 333000.0;
+    //Datos Tierra
+    double mt = 1.0;
+    double rt = 1.0;
+    double wt = sqrt(G*mS*pow(rt, -3));
+    double vt = wt*rt; //Orbita circular w*r 
+    //Datos Jupiter
+    double mj = 317.81;
+    double rj = 5.203;
+    double wj = sqrt(G*mS*pow(rj, -3));
+    double vj = wj*rj; //Orbita circular w*r 
+    //Datos Sonda
+    double ms = 4.225E-22;
+    double rs = rt + pow(mS, -2/5);
+    double v0s = sqrt((2*G*mS*rj)/(rs*(rs+rj))); //Velocidad de transicion
+    // double rs = rt + 4.25875E-5;
+    // double v0s = 0.783; //Velocidad de transicion
 
-    //Trayectoria circular(r=100, w=1/10, T=62.8)
-    //-----------( x0 ,y0, z0, Vx0, Vy0, Vz0,m)
-    Planeta Sol   (0,   0,  0, 0,    0,           0,  10000);
-    Planeta Tierra(50,  0,  0, 0,    10*sqrt(2),  0,  1);
-    Planeta Marte (100, 0,  0, 0,    10,          0,  1); 
+    //---(x0,y0,z0,Vx0,Vy0,Vz0,m)
+    Cuerpo Sol(0,0,0,0,0,0,mS);
+    Cuerpo Tierra(rt,0,0,0,vt,0,mt);
+    Cuerpo Jupiter(1.31619,5.03377,0,-0.2219,0.058,0,mj);
+    Cuerpo Sonda(rs,0,0,0,v0s,0, ms);
 
+    cuerpos N = {Sonda, Tierra, Jupiter, Sol};
     //Fuerza inicial
-    force(Sol, Tierra);
-
-    //Imprimir Leap_Frog
+    force(N);
     Sol.start_leap_frog(DT);
     Tierra.start_leap_frog(DT);
-    trans_galileo(Sol, Tierra);
-    print(Sol, Tierra, DT);
+    Jupiter.start_leap_frog(DT);
+    Sonda.start_leap_frog(DT);
+    trans_galileo(N);
+    print(N,0.0);
+
+    double time = 0.0;
 
     for(int ii = 0; ii < NSTEPS; ++ii){
-        Sol.leap_frog(DT);
-        Tierra.leap_frog(DT);
-        force(Sol,Tierra);
-        //trans_galileo(Sol, Tierra);
-        trans_galileo(Tierra, Sol);
-        print(Sol,Tierra, DT);
+        force(N);
+        trans_galileo(N);
+        print(N, DT*ii);
+        time = DT*ii;
+        //if(gassist(N, rj)){break;}
     }
 
-    //Imprimir Verlet
-    // for(int ii = 0; ii < NSTEPS; ii++){
-    //     Sol.verlet(DT);
-    //     Tierra.verlet(DT);
-    //     force(Sol,Tierra);
-    //     print(Sol,Tierra, DT);
-    // }
+    // double phi = acos(N[0].R.x/rj)-wj*time;
+    // std::cout << phi;
+
+    return 0;
 }
